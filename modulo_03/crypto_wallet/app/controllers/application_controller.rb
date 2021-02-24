3a876838c4a2c925ff94875025186d493721b25d
leandroalvesmachado <leandroalvesmachado@gmail.com>
2021-02-24 11:57:28 -0300

    Metade da aula 84

--- modulo_03/crypto_wallet/app/controllers/application_controller.rb
+++ modulo_03/crypto_wallet/app/controllers/application_controller.rb
@@ -1,2 +1,17 @@
 class ApplicationController < ActionController::Base
+    before_action :set_locale
+
+    def set_locale
+        # capturando parametro
+        if params[:locale]
+            cookies[:locale] = params[:locale]
+        end
+
+        if cookies[:locale]
+            if I18n.locale != cookies[:locale]
+                I18n.locale = cookies[:locale]
+            end
+        end
+    end
+
 end
--- modulo_03/crypto_wallet/app/controllers/welcome_controller.rb
+++ modulo_03/crypto_wallet/app/controllers/welcome_controller.rb
@@ -6,5 +6,10 @@ class WelcomeController < ApplicationController
 
     # Query Params para Requests, capturando params da url
     @nome_params = params[:nome] ? params[:nome] : 'sem query params'
+
+    # criando cookie
+    # mesmo comentando a linha, cookie tem uma duração
+    cookies[:curso] = "[COOKIE] Curso de Ruby on Rails - Leandro Alves"
+    session[:curso] = "[SESSION] Curso de Ruby on Rails - Leandro Alves"
   end
 end
