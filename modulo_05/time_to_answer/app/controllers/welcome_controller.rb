0f1e1832315e702400f4fb207ccecdef25b42d3b
leandroalvesmachado <leandroalvesmachado@gmail.com>
2021-03-06 15:24:11 -0300

    Iniciando aula 126

--- modulo_05/time_to_answer/app/controllers/welcome_controller.rb
+++ modulo_05/time_to_answer/app/controllers/welcome_controller.rb
@@ -1,4 +1,7 @@
 class WelcomeController < ApplicationController
-  def index
-  end
+    # autenticacao basica http
+    # http_basic_authenticate_with name: "dhh", password: "secret", except: :index
+
+    def index
+    end
 end
