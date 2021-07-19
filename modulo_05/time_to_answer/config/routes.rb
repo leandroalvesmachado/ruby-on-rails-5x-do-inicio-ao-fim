76820eb8de9e69414e269ae6fe1c07c488bb54a5
leandroalvesmachado <leandroalvesmachado@gmail.com>
2021-07-19 12:47:42 -0300

    Fim da aula 128

--- modulo_05/time_to_answer/config/routes.rb
+++ modulo_05/time_to_answer/config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
+  namespace :admins_backoffice do
+    get 'welcome/index'
+  end
+  namespace :profiles_backoffice do
+    get 'welcome/index'
+  end
+  namespace :admins_bacloffice do
+    get 'welcome/index'
+  end
   devise_for :profiles
   devise_for :admins
   root to: 'welcome#index'
