335f9c62354696dfa4fab37bb5099a2b44395d25
leandroalvesmachado <leandroalvesmachado@gmail.com>
2021-07-20 00:11:50 -0300

    Fim da aula 133

--- modulo_05/time_to_answer/app/controllers/welcome_controller.rb
+++
@@ -1,7 +0,0 @@
-class WelcomeController < ApplicationController
-    # autenticacao basica http
-    # http_basic_authenticate_with name: "dhh", password: "secret", except: :index
-
-    def index
-    end
-end
--- modulo_05/time_to_answer/config/routes.rb
+++ modulo_05/time_to_answer/config/routes.rb
@@ -1,16 +1,20 @@
 Rails.application.routes.draw do
+  
+  namespace :site do
+    get 'welcome/index'
+  end
   namespace :admins_backoffice do
     get 'welcome/index'
   end
-  namespace :profiles_backoffice do
+  namespace :users_backoffice do
     get 'welcome/index'
   end
-  namespace :admins_bacloffice do
-    get 'welcome/index'
-  end
-  devise_for :profiles
+  
   devise_for :admins
-  root to: 'welcome#index'
-  get 'welcome/index'
+  devise_for :users
+
+  get 'inicio', to: 'site/welcome#index'
+
+  root to: 'site/welcome#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
